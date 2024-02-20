19db180ffccd69b6c62c024df2fa94d5cdf7fce4
kosder7645 <kostrowka123@gmail.com>
2024-02-20 17:32:39 +0100

    level coloring and level standarization

--- src/tempest/flipper.ts
+++ src/tempest/flipper.ts
@@ -3,31 +3,29 @@ import { MyGame } from "../main";
 
 export default class Fipper extends PhysicalGameObject {
   game: MyGame;
-  constructor( options: PhysicalObjectInitialConfig, game: MyGame) {
+  constructor(options: PhysicalObjectInitialConfig, game: MyGame) {
     super(`obj/flipper.obj`, options);
     this.game = game;
     this.loadMesh().then(() => {
-
       for (let i = 0; i < this.getMesh().length; i++) {
         this.setLineColor(i, "red");
       }
     });
   }
-  override Start(): void {
+  override Start(): void {}
 
-  }
-
-  static createFlipper(game: MyGame){
+  static createFlipper(game: MyGame) {
     if (game.currentScene == null) {
-        throw new Error("Main scene must be set first.");
-      }
+      throw new Error("Main scene must be set first.");
+    }
     const flipper = new Fipper({ position: [0, 0, 0], size: [1, 1, 1] }, game);
     game.currentScene.addGameObject(flipper);
     game.flippers.push(flipper);
-    // console.log(flipper.vertecies)
+    console.log(flipper.vertecies);
     // flipper.vertecies[2] = game.level.vertecies[Math.floor(game.currentLevelSide)];
     // flipper.vertecies[2].z = 50;
     // flipper.vertecies[3] = game.level.vertecies[(Math.floor(game.currentLevelSide) + 1) % game.numberOfSides] ;
     // flipper.vertecies[3].z = 50;
   }
+  setFlipperPosition() {}
 }
